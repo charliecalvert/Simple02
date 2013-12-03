e00167dfac3ed39583b097d2de714f220c33be15
Charles Calvert <java@elvenware.com>
2013-12-03 15:10:57 -0800

    Add SendToS3 and config.json

--- Week09Android/Server.js
+++ Week09Android/Server.js
@@ -1,108 +1,183 @@
+/* 
+	Server.js by Charlie Calvert 
+	v0.2.1
+*/
+
 var http = require('http');
 var url = require('url');
 var port = process.env.PORT || 30025;
 var fs = require('fs');
 var path = require('path');
+var lastHtmlFile = "";
 
-function getPath(request) {
+function getPath(request) { 'use strict';
 	return url.parse(request.url).pathname;
 }
 
 // Thanks to Wallace: http://stackoverflow.com/a/1203361/253576
-function getExtension(fileName) {
+function getExtension(fileName) { 'use strict';
 	var fileNameArray = fileName.split(".");
-	if( fileNameArray.length === 1 || ( fileNameArray[0] === "" && fileNameArray.length === 2 ) ) {
+	if (fileNameArray.length === 1 || (fileNameArray[0] === "" && fileNameArray.length === 2 )) {
 		return "";
 	}
-	return fileNameArray.pop().toLowerCase();    
+	return fileNameArray.pop().toLowerCase();
 }
 
 // Add endsWith method to String
-if (typeof String.prototype.endsWith !== 'function') {
-    String.prototype.endsWith = function(suffix) {
-        return this.indexOf(suffix, this.length - suffix.length) !== -1;
-    };
+if ( typeof String.prototype.endsWith !== 'function') {
+	String.prototype.endsWith = function(suffix) { 'use strict';
+		return this.indexOf(suffix, this.length - suffix.length) !== -1;
+	};
 }
 
-var findFile = function(dir, fileName, done) {
+var findFile = function(dir, fileName, done) { 'use strict';
 	var results = [];
 	var ext = path.extname(fileName);
-	fs.readdir(dir, function(err, list) {
+	try {
+		fs.readdir(dir, function(err, list) {
 
-	if (err) return done(err);
-	var pending = list.length;
-	if (!pending) return done(null, results);
-	list.forEach(function(file) {
-		file = dir + '/' + file;
-		fs.stat(file, function(err, stat) {
-			if (stat && stat.isDirectory()) {
-				findFile(file, fileName, function(err, res) {
-				results = results.concat(res);
-				if (!--pending) done(null, results);
-			});
-		} else {
-			if (path.extname(file) === ext) {
-				console.log(path.basename(file) + " -- " + fileName);
-				if (file.endsWith(fileName)) {
-					file = file.replace(/\\/g, '\/');
-					results.push(file);
-				}
+			if (err) {
+				console.log("ERROR!!!!");
+				return done(err);
 			}
-			if (!--pending) done(null, results);
-		}
-	  });
-	});
-  });
+			var pending = list.length;
+			if (!pending)
+				return done(null, results);
+			try {
+				list.forEach(function(file) {
+					file = dir + '/' + file;
+					fs.stat(file, function(err, stat) {
+						if (stat && stat.isDirectory()) {
+							findFile(file, fileName, function(err, res) {
+								results = results.concat(res);
+								if (!--pending)
+									done(null, results);
+							});
+						} else {
+							//console.log("Pending: " + pending + " File " + file);
+							var newExt = path.extname(file);
+							if (newExt !== "" && newExt === ext) {
+								if (file.endsWith(fileName)) {
+									file = file.replace(/\\/g, '\/');
+									results.push(file);
+								}
+							} else {
+								//console.log("Rejecting: " + file + " FileName " + fileName);
+							}
+							if (!--pending) {
+								done(null, results);
+							}
+						}
+					});
+				});
+			} catch(e) {
+				throw e;
+			}
+		});
+	} catch(e) {
+		console.log("Can't read");
+	}
 };
 
-function loadContent(request, response) {
+function getFile(fileName) { 'use strict';
+	try {
+		return fs.readFileSync(fileName);
+	} catch(e) {
+		console.log('***** ELF ERROR REPORT *****');
+		console.log(e.name);
+		console.log('Can not open: ' + fileName);
+		console.log(e.message);
+		console.log('***** END ERROR REPORT *****');
+		return null;
+	}
+}
+
+function getBinaryFile(fileName, response) { 'use strict';
+	try {
+		fs.readFile(fileName, "binary", function(err, path) {
+			if (err) {
+				console.log('***** ELF BINARY ERROR REPORT *****');
+				console.log(err.name);
+				console.log("Last HTML file: " + lastHtmlFile);
+				console.log('Can not open binary file: ' + path);
+				console.log(err.message);
+				console.log('***** END ERROR REPORT *****');
+				console.log("Error reading binary file");
+				process.exit(1);
+			} else {
+				response.writeHeader(200, {
+					"Content-Type" : "image/png"
+				});
+				response.write(path, "binary");
+				response.end();
+			} 
+		}); 
+	} catch(e) {
+		console.log('***** ELF BINARY ERROR REPORT *****');
+		console.log(e.name);
+		console.log('Can not open binary file: ' + fileName);
+		console.log(e.message);
+		console.log('***** END ERROR REPORT *****');		
+	}
+}
+
+function getType(ext) { 'use strict';
+	switch (ext) {
+		case 'css': 
+			return 'text/css';
+		case 'html':
+		case 'htm':
+			return 'text/html';
+		case 'js':		
+			return 'text/javascript';
+		case 'json':
+			return 'application/json';
+		default:
+			throw 'Unknown type: ' + ext;
+	}
+}
+
+function setLastHtml(path, ext) { 'use strict';
+	if (ext === 'html' || ext === 'htm') {
+		lastHtmlFile = path;
+	}
+}
+
+function loadContent(request, response) { 'use strict';
 	var path = getPath(request);
+	var ext = getExtension(path);
 	console.log("Request for " + path + " received.");
-	if (getExtension(path) === 'css') {
+	if (ext === 'css' || ext === 'html' || ext === 'htm' || ext === 'js' || ext === 'json') {
+		setLastHtml(path, ext);
 		findFile(__dirname, path.replace('\/', ''), function(err, results) {
-			console.log("New Path: " + results[0]);
-			var css = fs.readFileSync(results[0]);
-			response.writeHead(200, {'Content-Type': 'text/css'});
-			response.write(css);
-			response.end();
+			console.log("Found: " + path);
+			var css = getFile(results[0]);
+			if (css === null) {
+				throw "Can't find: " + path;
+			} else {
+				response.writeHead(200, {
+					'Content-Type' : getType(ext)
+				});
+				response.write(css);
+				response.end();
+			}
 		});
-	} else if (getExtension(path) === 'html') {
-		var html = fs.readFileSync(__dirname + path);
-		response.writeHead(200, {'Content-Type': 'text/html'});
-		response.write(html);
-		response.end();
-	} else if (getExtension(path) === 'js') {
+	} else if (getExtension(path) === 'png' || getExtension(path) === 'gif' || getExtension(path) === 'jpg') {
 		findFile(__dirname, path.replace('\/', ''), function(err, results) {
-			var javaScript = fs.readFileSync(results[0]);
-			response.writeHead(200, {'Content-Type': 'text/javascript'});
-			response.write(javaScript);
-		response.end();
-		});
-	} else if (getExtension(path) === 'png' || getExtension(path) === 'gif' || getExtension(path) === 'jpg' ) {
-		findFile(__dirname, path.replace('\/', ''), function(err, results) {
-			fs.readFile(results[0], "binary", function(err, file) {
-				console.log("PNG detected");
-				if(err) {
-					console.log("Error reading binary file");
-					response.writeHeader(500, {"Content-Type": "text/plain"});
-					response.write(err + "\n");
-					response.end();
-				}
-				else{
-					console.log("PNG loaded");
-					response.writeHeader(200, {"Content-Type": "image/png"});
-					response.write(file, "binary");
-					response.end();
-				}
-			});
+			console.log("Found: " + path);
+			getBinaryFile(results[0], response);
 		});
 	} else {
-	    var html = fs.readFileSync(__dirname + '/index.html');
-		response.writeHead(200, {'Content-Type': 'text/html'});
+		lastHtmlFile = '/index.html';
+		var html = fs.readFileSync(__dirname + lastHtmlFile);
+		response.writeHead(200, {
+			'Content-Type' : 'text/html'
+		});
 		response.write(html);
 		response.end();
 	}
 }
 
+
 http.createServer(loadContent).listen(port);
-console.log("Server has started:" + port);
+console.log("Server has started on port: " + port);
