32425e138f18532e5bbe00d88e09e30b499138f4
Charlie CedarIsle Calvert <charlie@elvenware.com>
2014-01-26 17:00:27 -0800

    Bootstrap starter

--- MilesFunction/test01.js
+++ MilesFunction/test01.js
@@ -1,27 +1,28 @@
 /**
- * @author Chris
+ * @author Charlie Calvert
  */
 
-describe("Mile Function Project", function() {
+describe("Miles Function Project", function() {
 	'use strict';
-	it("expects true to be true", function() {
-		expect(true).toBe(true);
+	
+	it ("can create milesFunction", function() {
+		expect(new milesFunction()).toBeTruthy();
 	});
-
+	
 	it("converts 3 miles into feet", function() {
-		expect(milesConvert.milesConvert(3)).toBe(15840);
+		expect(new milesFunction().milesConvert(3)).toBe(15840);
 	});
 
 	it("converts 0 miles into feet", function() {
-		expect(milesConvert.milesConvert(0)).toBe(0);
+		expect(new milesFunction().milesConvert(0)).toBe(0);
 	});
 
 	it("converts 10 miles into feet", function() {
-		expect(milesConvert.milesConvert(10)).toBe(52800);
+		expect(new milesFunction().milesConvert(10)).toBe(52800);
 	});
 
 	it("converts -3 miles into feet", function() {
-		expect(milesConvert.milesConvert(-3)).toBe(-15840);
+		expect(new milesFunction().milesConvert(-3)).toBe(-15840);
 	});
 
 });
