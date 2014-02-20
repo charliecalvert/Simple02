3b7207f6fac6f33fc9f047f25fcbc8482de3ca44
Charlie CedarIsle Calvert <charlie@elvenware.com>
2014-02-19 20:28:18 -0800

    Straightening out the names

--- Cordova/CordovaInput/gen/com/elvenware/cordovainput/R.java
+++ Cordova/CordovaInput/gen/com/elvenware/cordovainput/R.java
@@ -5,7 +5,7 @@
  * should not be modified by hand.
  */
 
-package com.elvenware.testm01;
+package com.elvenware.cordovainput;
 
 public final class R {
     public static final class attr {
--- Cordova/CordovaInput/src/com/elvenware/cordovainput/CordovaInput.java
+++ Cordova/CordovaInput/src/com/elvenware/cordovainput/CordovaInput.java
@@ -17,12 +17,12 @@
        under the License.
  */
 
-package com.elvenware.testm01;
+package com.elvenware.cordovainput;
 
 import android.os.Bundle;
 import org.apache.cordova.*;
 
-public class TestMe01 extends CordovaActivity 
+public class CordovaInput extends CordovaActivity 
 {
     @Override
     public void onCreate(Bundle savedInstanceState)
