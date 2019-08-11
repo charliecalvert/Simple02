fad849910c53393ff4a3d3617f07c37e71dd4b71
Charlie on Linux Ubuntu Main <charlie@elvenware.com>
2019-08-10 17:01:08 -0700

    ncu

--- Simple02/Simple02.js
+++ Simple02/Simple02.js
@@ -1,30 +1,27 @@
 var http = require('http');
-var port = process.env.C9_PORT || 1337;
+var port = process.env.PORT || 30025;
 var fs = require('fs');
 
 // A Comment I just added
-function run(res)
-{
-	function bar()
-	{
-		return "Margie and Charlie 03 on port: " + port;
-	}
+function run(res) {
+    function bar() {
+        return 'Margie and Charlie 03 on port: ' + port;
+    }
 
-	fs.readFile('./Data.html', function (err, html) {
-		if (err) {
-			throw err; 
-		}   
-		res.writeHead(200, { 'Content-Type': 'text/html' });
-		res.write('<h1>Bar Foo 01</h1>');
-		res.write('<p>' + bar() + '</p>');
-		res.write(html);
-		res.end('<p>Hello HTTP from the JSNode\n</p>');
-	});
+    fs.readFile('./Data.html', function(err, html) {
+        if (err) {
+            throw err;
+        }
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.write('<h1>Bar Foo 01</h1>');
+        res.write('<p>' + bar() + '</p>');
+        res.write(html);
+        res.end('<p>Hello HTTP from the JSNode\n</p>');
+    });
 }
 
-var server = http.createServer(function (req, res) {
-	run(res);
+var server = http.createServer(function(req, res) {
+    run(res);
 });
-
 
 server.listen(port);
